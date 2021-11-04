112f58e3d723ea61779ec43c29c0067142540346
ThongkornW <conun_korn@hotmail.com>
2021-11-05 01:40:40 +0700

    fix_confrim-email
    fix_confrim-email

--- resources/views/confirm-mail.blade.php
+++ resources/views/confirm-mail.blade.php
@@ -1,58 +1,72 @@
 <html>
 <head>
-    <title>ticket-1-01</title>
+    <title>NT โทรคมนาคมแห่งชาติ-ขอบคุณสำหรับการลงทะเบียน</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
 <!-- Save for Web Slices (ticket-1-01.jpg) -->
-<table id="Table_01" width="650" height="904" border="0" cellpadding="0" cellspacing="0" align="center">
+<table id="Table_01" width="650" height="903" border="0" cellpadding="0" cellspacing="0" align="center" style="font-size: 14px;">
     <tr>
-        <td colspan="6">
-            <img src="{{ asset('images/confirm/confirm_01.jpg') }}" width="650" height="422" alt=""></td>
-    </tr>
-    <tr>
-        <td colspan="2">
-            <img src="{{ asset('images/confirm/confirm_02.jpg') }}" width="114" height="25" alt=""></td>
-        <td>{{ $data->formatted_id }}</td>
-        <td>
-            <img src="{{ asset('images/confirm/confirm_04.jpg') }}" width="77" height="25" alt=""></td>
-        <td>{{ $data->full_name }}</td>
-        <td>
-            <img src="{{ asset('images/confirm/confirm_06.jpg') }}" width="73" height="25" alt=""></td>
-    </tr>
-    <tr>
-        <td colspan="6">
-            <img src="{{ asset('images/confirm/confirm_07.jpg') }}" width="650" height="41" alt=""></td>
+        <td colspan="5">
+            <img src="{{ asset('images/confirm/index_01.jpg') }}" width="650" height="411" alt=""></td>
     </tr>
     <tr>
         <td>
-            <img src="{{ asset('images/confirm/confirm_08.jpg') }}" width="58" height="107" alt=""></td>
-        <td colspan="4" align="left">Join Zoom Meeting: <a href="https://zoom.us/j/92887006155?pwd=aWIzM1VJZi9MMHB2SFU1ZUtEeWdhUT09" target="_blank">https://zoom.us/j/92887006155?pwd=aWIzM1VJZi9MMHB2SFU1ZUtEeWdhUT09</a>
-            <br />
-            Meeting ID: 928 8700 6155
-            <br />
-            Passcode: 209915
+            <img src="{{ asset('images/confirm/index_02.jpg') }}" width="32" height="15" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_03.jpg') }}" width="28" height="15" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_04.jpg') }}" width="520" height="15" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_05.jpg') }}" width="34" height="15" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_06.jpg') }}" width="36" height="15" alt=""></td>
+    </tr>
+    <tr>
+        <td width="32" style="background-color: #FFD101;">
+            <img src="{{ asset('images/confirm/index_07.jpg') }}" width="32" height="16" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_08.jpg') }}" width="28" height="16" alt=""></td>
+        <td>
+            <div style="width: 48%; float:left;">ID No : {{ $data->formatted_id }}</div>
+            <div style="width: 50%; float:right;border-left: 1px solid #ccc;padding-left: 8px;">Name : {{ $data->full_name }}</div>
         </td>
         <td>
-            <img src="{{ asset('images/confirm/confirm_10.jpg') }}" width="73" height="107" alt=""></td>
+            <img src="{{ asset('images/confirm/index_10.jpg') }}" width="34" height="16" alt=""></td>
+        <td width="36" style="background-color: #FFD101;">
+            <img src="{{ asset('images/confirm/index_11.jpg') }}" width="36" height="16" alt=""></td>
     </tr>
     <tr>
-        <td colspan="6">
-            <img src="{{ asset('images/confirm/confirm_11.jpg') }}" width="650" height="308" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_12.jpg') }}" width="32" height="48" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_13.jpg') }}" width="28" height="48" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_14.jpg') }}" width="520" height="48" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_15.jpg') }}" width="34" height="48" alt=""></td>
+        <td>
+            <img src="{{ asset('images/confirm/index_16.jpg') }}" width="36" height="48" alt=""></td>
     </tr>
     <tr>
+        <td width="32" style="background-color: #FFD101;">
+            <img src="{{ asset('images/confirm/index_17.jpg') }}" width="32" height="103" alt=""></td>
         <td>
-            <img src="{{ asset('images/confirm/spacer.gif') }}" width="58" height="1" alt=""></td>
+            <img src="{{ asset('images/confirm/index_18.jpg') }}" width="28" height="103" alt=""></td>
+        <td align="left">Join Zoom Meeting : <a href="https://zoom.us/j/92887006155?pwd=aWIzM1VJZi9MMHB2SFU1ZUtEeWdhUT09" target="_blank">https://zoom.us/j/92887006155?pwd=aWIzM1VJZi9MMHB2SFU1ZUtEeWdhUT09</a>
+            <br />
+            Meeting ID : 928 8700 6155
+            <br />
+            Passcode : 209915
+        </td>
         <td>
-            <img src="{{ asset('images/confirm/spacer.gif') }}" width="56" height="1" alt=""></td>
-        <td>
-            <img src="{{ asset('images/confirm/spacer.gif') }}" width="205" height="1" alt=""></td>
-        <td>
-            <img src="{{ asset('images/confirm/spacer.gif') }}" width="77" height="1" alt=""></td>
-        <td>
-            <img src="{{ asset('images/confirm/spacer.gif') }}" width="181" height="1" alt=""></td>
-        <td>
-            <img src="{{ asset('images/confirm/spacer.gif') }}" width="73" height="1" alt=""></td>
+            <img src="{{ asset('images/confirm/index_20.jpg') }}" width="34" height="103" alt=""></td>
+        <td width="36" style="background-color: #FFD101;">
+            <img src="{{ asset('images/confirm/index_21.jpg') }}" width="36" height="103" alt=""></td>
+    </tr>
+    <tr>
+        <td colspan="5">
+            <img src="{{ asset('images/confirm/index_22.jpg') }}" width="650" height="310" alt=""></td>
     </tr>
 </table>
 <!-- End Save for Web Slices -->
