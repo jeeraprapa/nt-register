899ea04d8783c938386695d2c5894db60b666771
jeeraprapa <jeeraprapa.h@gmail.com>
2021-11-05 22:02:53 +0700

    change reminder mail

--- resources/views/admin/emails/reminder.blade.php
+++ resources/views/admin/emails/reminder.blade.php
@@ -1,1 +1,33 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="th-th">
+<body>
+<p>
+    This is an automatic reminder of the following webinar By NT.
+</p>
+<p>
+    “Recharge & Comeback Stronger”
+</p>
+<p>
+    <strong>Time :</strong>
+    <br />
+    September 17,2021
+    <br />
+    1.00-4.30 p.m.
+</p>
+<p>
+    <strong>Join Zoom Meeting:</strong> <a href="https://zoom.us/j/92887006155?pwd=aWIzM1VJZi9MMHB2SFU1ZUtEeWdhUT09" target="_blank">https://zoom.us/j/92887006155?pwd=aWIzM1VJZi9MMHB2SFU1ZUtEeWdhUT09</a>
+    <br />
+    Meeting ID : 928 8700 6155
+    <br />
+    Passcode : 209915
+</p>
+
+<p>
+    We look forward to seeing you!
+</p>
+<br />
+<p>
+    Sincerely,
+</p>
+</body>
+</html>
