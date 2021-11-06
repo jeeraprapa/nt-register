453b55665baf863255b5766eecf7364f890b72a1
jeeraprapa <jeeraprapa.h@gmail.com>
2021-11-06 13:29:56 +0700

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,10 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        if($this->app->environment('production')){
-            \URL::forceScheme('https');
-            \Log::info('https');
-        }
+
     }
 
     /**
@@ -26,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if($this->app->environment('production')){
+            \URL::forceScheme('https');
+            \Log::info('https');
+        }
     }
 }
