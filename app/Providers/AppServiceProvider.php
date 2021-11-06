122e16c63d56bd217bb583aafcdaf137cf36fc23
jeeraprapa <jeeraprapa.h@gmail.com>
2021-11-06 13:07:31 +0700

    add https protocl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,6 +15,7 @@ class AppServiceProvider extends ServiceProvider
     {
         if($this->app->environment('production')){
             \URL::forceScheme('https');
+            \Log::info('https');
         }
     }
 
